3d45dd99bc218060138192e0b5c247985fd9e720
Mark Hennen <mark.hennen@hotmail.co.uk>
2025-10-10 16:12:33 +0100

    fix:update file paths to rasters using remote git repository

--- pages/1_Maps.py
+++ pages/1_Maps.py
@@ -58,21 +58,22 @@ st.sidebar.image(gr_logo)
 # -----------------------------------------------------------
 # Rasters Config
 # -----------------------------------------------------------
+url_path = "https://raw.githubusercontent.com/M-hennen/streamlit_PCI/main/data2/"
 ndvi_rasters = {
-    i: [f'{year}_{season}', f'data2/seasons/Abergavenny_NDVI_{year}_{season}_harm_cog.tif']
+    i: [f'{year}_{season}', f'{url_path}seasons/Abergavenny_NDVI_{year}_{season}_harm_cog.tif']
     for i, (year, season) in enumerate(
         ( (year, season) for year in range(2017, 2026) for season in ["Spring", "Summer", "Autumn", "Winter"] )
     )
 }
-ndvi_rasters[34] = ['Latest_NDVI', 'data2/latest/Abergavenny_NDVI_2025-09-30_latest_harm_cog.tif']
+ndvi_rasters[34] = ['Latest_NDVI', f'{url_path}latest/Abergavenny_NDVI_2025-09-30_latest_harm_cog.tif']
 
 s1_rasters = {
-    i: [f'{year}_{season}', f'data2/seasons/Abergavenny_S1_{year}_{season}_harm_cog.tif']
+    i: [f'{year}_{season}', f'{url_path}seasons/Abergavenny_S1_{year}_{season}_harm_cog.tif']
     for i, (year, season) in enumerate(
         ( (year, season) for year in range(2017, 2026) for season in ["Spring", "Summer", "Autumn", "Winter"] )
     )
 }
-s1_rasters[34] = ['Latest_Radar', 'data2/latest/Abergavenny_S1_VV_2025-10-02_latest_harm_cog.tif']
+s1_rasters[34] = ['Latest_Radar', f'{url_path}latest/Abergavenny_S1_VV_2025-10-02_latest_harm_cog.tif']
 
 symb_dict = {
     "NDVI": ["rdylgn", (0.2, 0.8)],
@@ -173,8 +174,8 @@ with col2:
         if selected_date == compare_date:
             st.warning("Pick two different dates.")
         else:
-            before_path = dataset[selected_date][1]
-            after_path  = dataset[compare_date][1]
+            before_path = "data2/latest/Abergavenny_NDVI_2025-09-30_latest_harm_cog.tif" #'dataset[selected_date][1]
+            after_path  = "data2/seasons/Abergavenny_NDVI_2020_Spring_harm_cog.tif" #'dataset[compare_date][1]
 
             ndvi_before, prof = load_ndvi(before_path)
             ndvi_after, _ = load_ndvi(after_path)
@@ -200,91 +201,45 @@ with col2:
     date_id = ndvi_rasters[selected_date][0]
     st.write(f"Displaying rasters for: {date_id}")
 
-    # m = leafmap.Map(basemap="Esri.WorldImagery")
-
-    # # Add basemap
-    # esri_satellite = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-    # m.add_tile_layer(url=esri_satellite, name="Esri Satellite", attribution="Tiles © Esri")
-
-    # Use the TiTiler demo endpoint. Replace this if needed.
-    # os.environ["TITILER_ENDPOINT"] = "https://giswqs-titiler-endpoint.hf.space"
-
-    # test_url = "https://github.com/M-hennen/streamlit_PCI/blob/main/data2/latest/Abergavenny_NDVI_2025-09-30_latest_harm_cog.tif"
-    # test_url = "https://raw.githubusercontent.com/M-hennen/streamlit_PCI/main/data2/latest/Abergavenny_NDVI_2025-09-30_latest_harm_cog.tif"
-    # m.add_cog_layer(
-    #     test_url, bands=[0])
-    # m    
-
     os.environ["TITILER_ENDPOINT"] = "https://giswqs-titiler-endpoint.hf.space"
 
-    m = leafmap.Map(center=[51.787, -3.023], zoom=14, basemap="Esri.WorldImagery")
+    m = leafmap.Map(center=[51.787, -3.023], basemap="Esri.WorldImagery")
 
-    test_url = "https://raw.githubusercontent.com/M-hennen/streamlit_PCI/main/data2/latest/Abergavenny_NDVI_2025-09-30_latest_harm_cog.tif"
+    # # Add basemap
+    esri_satellite = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+    m.add_tile_layer(url=esri_satellite, name="Esri Satellite", attribution="Tiles © Esri")
+    import requests
+    resp = requests.head(dataset[selected_date][1])
+    if resp.status_code == 200:
+        test_url = dataset[selected_date][1]  
+        print("Loading COG from:", test_url)  
+        # Single-band grayscale
+        m.add_cog_layer(
+            test_url,
+            bands=[1],
+            name="NDVI (COG)",
+            colormap_name="rdylgn"
+        )
+        if st.session_state.change_detection and selected_date != compare_date:
+            m.add_raster(
+                delta_path,
+                bands=[1],
+                vmin=-np.nanmax(np.abs(delta)),
+                vmax=np.nanmax(np.abs(delta)),
+                colormap="rdylgn",
+                layer_name=f"ΔNDVI Change",
+                opacity=1,
+            )
 
-    # Single-band grayscale
-    m.add_cog_layer(
-        test_url,
-        bands=[1],
-        name="NDVI (COG)",
-        colormap_name="rdylgn"
-    )
 
+    else: 
+        st.error(f"{choice} data not available for {date_id}")
+
+    region = "data2/TR0001_01_TR0001_01_boundary.geojson"
+    m.add_layer_control()
+    m.add_geojson(region, layer_name="AOI")
+    m.zoom_to_bounds(ndvi_rasters[selected_date][1])
     m.to_streamlit(height=500)
-
-
-    # if not os.path.exists(dataset[selected_date][1]):
-    #     st.error(f"Raster file not found: {dataset[selected_date][1]}")
-    # else:
-        # with rasterio.open(dataset[selected_date][1]) as src:
-        #     data = src.read(1)
-        #     profile = src.profile
-
-        # # Normalize the data to 0–255 for display
-        # arr = data.astype(float)
-        # arr_min, arr_max = np.nanmin(arr), np.nanmax(arr)
-        # scaled = ((arr - arr_min) / (arr_max - arr_min) * 255).astype(np.uint8)
-
-        # # Convert to PIL image
-        # img = Image.fromarray(scaled)
-
-        # # Show the image
-        # st.image(img, caption="band2 (COG)", use_column_width=True)
-
-        
-
-        # m = folium.Map(location=[34.17, 78.01], zoom_start=12)
-        # folium.GeoJson("https://your-url.com/path/to/boundary.geojson").add_to(m)
-
-        # st_data = st_folium(m, width=700, height=500)
-
-
-        # # Add main raster
-        # m.add_raster(
-        #     test_url,
-        #     # dataset[selected_date][1],
-        #     bands=[1],
-        #     vmin=symb_dict[choice][1][0], 
-        #     vmax=symb_dict[choice][1][1], 
-        #     colormap=symb_dict[choice][0], 
-        #     layer_name=f"{choice} {date_id}",
-        #     opacity=0.7,
-        # )
-        # if st.session_state.change_detection and selected_date != compare_date:
-        #     m.add_raster(
-        #         delta_path,
-        #         bands=[1],
-        #         vmin=-np.nanmax(np.abs(delta)),
-        #         vmax=np.nanmax(np.abs(delta)),
-        #         colormap="rdylgn",
-        #         layer_name=f"ΔNDVI Change",
-        #         opacity=1,
-        #     )
-
-    # region = "data2/TR0001_01_TR0001_01_boundary.geojson"
-    # m.add_geojson(region, layer_name="AOI")
-    # m.add_layer_control()
-    # m.zoom_to_bounds(ndvi_rasters[selected_date][1])
-    # m.to_streamlit(height=500)
 
     st.markdown(
         """
